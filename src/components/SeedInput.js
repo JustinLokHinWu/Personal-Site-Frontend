139ee5991fc17e1d1252c99e17851464a94dd4d6
Justin Wu <justin4827@gmail.com>
2021-08-16 20:41:14 -0400

    Cleaned up seed input

--- src/components/SeedInput.js
+++ src/components/SeedInput.js
@@ -2,10 +2,6 @@ import { Input } from 'antd'
 import "antd/dist/antd.css";
 
 const SeedInput = ({seed, setSeed}) => {
-    const validate = (value) => {
-        
-    }
-
     const onChange = (e) => {
         const value = e.target.value
         const reg = /^[0-9]*$/
@@ -14,10 +10,7 @@ const SeedInput = ({seed, setSeed}) => {
         }
         else if (reg.test(value) && +value < Number.MAX_SAFE_INTEGER){
             setSeed(value)
-        } else {
-            return false
         }
-
     }
 
     return (
