87ad5e857c7f08b3f69277e80a1ef21e04a05b51
Justin Wu <justin4827@gmail.com>
2022-02-23 18:37:44 -0500

    Removed unused code

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -13,12 +13,11 @@ const Biography = () => {
         >
             <Avatar
                 size={{ xs: 128, sm: 160, md: 192, lg: 192, xl: 224, xxl: 256 }}
-                style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }}
+                style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '25px', marginBottom: '10px' }}
             />
             <Title level={3}>Justin Wu</Title>
             <Title level={5}>Software Developer</Title>
             <Socials />
-            <Divider />
             <Paragraph style={{'textAlign': 'justify'}}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ex nulla, tincidunt sed faucibus eget, dignissim eu ligula. Vivamus congue erat egestas tempor vestibulum. Duis pharetra felis lacinia imperdiet viverra. Fusce egestas, mauris a cursus suscipit, turpis mi imperdiet lectus, et fermentum ipsum massa in elit. Nulla neque massa, pellentesque ut urna eget, hendrerit mollis diam. Cras lobortis quam vel posuere tempor. Fusce semper cursus dolor, et varius tortor commodo sit amet. Nam sit amet orci ac turpis imperdiet consequat. Duis eget neque eu libero ultrices posuere. Sed vestibulum dui risus, non vulputate diam pretium ut. Proin vel lectus ex. Mauris lobortis nunc non libero dictum, eu pretium est consectetur. Quisque dapibus mi sit amet pharetra volutpat.
             </Paragraph>
