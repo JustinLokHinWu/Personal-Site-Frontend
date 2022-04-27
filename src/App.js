06ec9858cbbb783a9ada94b9a299edd452f9f842
Justin Wu <justin4827@gmail.com>
2022-04-27 17:42:43 -0400

    Added app and header animations

--- src/App.js
+++ src/App.js
@@ -5,28 +5,48 @@ import HeaderMenu from './components/HeaderMenu';
 import ContentRoutes from './components/ContentRoutes';
 import Socials from './components/Socials'
 import PageFooter from './components/PageFooter';
+import { motion } from 'framer-motion/dist/framer-motion'
 
 const { Content } = Layout;
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Layout style={{ 'minHeight': '100vh'}}>
-        <Content className="site-layout">
-          <Affix>
-            <HeaderMenu />
-          </Affix>
-          <Col
-            xs={24} sm={24} md={20} lg={18} xl={16}
-            style={{ padding: '30px 5%', minHeight: 380, margin: 'auto'}}
-          >
-            <ContentRoutes />
-          </Col>
-          </Content>
-        <PageFooter />
-      </Layout>
-    </BrowserRouter>
-  );
+    const contentVariant = {
+        'hidden': {
+            opacity: 0
+        },
+        'visible': {
+            opacity: 1,
+            transition: {
+                duration: 0.5,
+                when: 'beforeChildren'
+            }
+        }
+    }
+
+    return (
+        <BrowserRouter>
+            <Layout style={{ 'minHeight': '100vh'}}>
+                <motion.div
+                    variants={contentVariant}
+                    initial='hidden'
+                    animate='visible'
+                    >
+                    <Content className="site-layout">
+                        <Affix>
+                            <HeaderMenu />
+                        </Affix>                  
+                            <Col
+                            xs={24} sm={24} md={20} lg={18} xl={16}
+                            style={{ padding: '30px 5%', minHeight: 380, margin: 'auto'}}
+                            >
+                                <ContentRoutes />
+                            </Col>
+                    </Content>
+                </motion.div>
+                <PageFooter />
+            </Layout>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -1,4 +1,5 @@
 import { Menu } from 'antd'
+import { motion } from 'framer-motion/dist/framer-motion'
 
 import { HashLink as Link } from 'react-router-hash-link'
 import { HomeOutlined } from '@ant-design/icons'
@@ -27,36 +28,54 @@ const HeaderMenu = () => {
         }
     ]
 
-    return (
-        <Menu
-            theme='dark'
-            mode='horizontal'
-        >
-            <Menu.Item
-                key='home'>
-                <Link
-                    smooth
-                    to='/#'><HomeOutlined /></Link>
-            </Menu.Item>
-            {
-                entries.map(entry => 
-                    <Menu.Item
-                        key={entry.key}>
-                        <Link
-                            scroll={(el) => {
-                                window.scroll({
-                                    top: el.offsetTop,
-                                    left: 0,
-                                    behavior: 'smooth'
-                                })
-                            }}
-                            to={entry.to}>
-                            {entry.text}
-                        </Link>
-                    </Menu.Item>
-                )
+    const headerVariant = {
+        'hidden': {
+            y: -30,
+            opacity: 0
+        },
+        'visible': {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.4,
             }
-        </Menu>
+        }
+    }
+
+    return (
+        <motion.div
+            variants={headerVariant}
+            >
+            <Menu
+                theme='dark'
+                mode='horizontal'
+            >
+                <Menu.Item
+                    key='home'>
+                    <Link
+                        smooth
+                        to='/#'><HomeOutlined /></Link>
+                </Menu.Item>
+                {
+                    entries.map(entry => 
+                        <Menu.Item
+                            key={entry.key}>
+                            <Link
+                                scroll={(el) => {
+                                    window.scroll({
+                                        top: el.offsetTop,
+                                        left: 0,
+                                        behavior: 'smooth'
+                                    })
+                                }}
+                                to={entry.to}>
+                                {entry.text}
+                            </Link>
+                        </Menu.Item>
+                    )
+                }
+            </Menu>
+        </motion.div>
     )
 }
 
