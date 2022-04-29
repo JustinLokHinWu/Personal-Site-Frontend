a6fa4697eea38783b08077e4eadd22e5600b7fb3
Justin Wu <justin4827@gmail.com>
2022-04-29 03:32:54 -0400

    Fixed breaking eslint issues

--- src/components/Articles/ActganArticle.jsx
+++ src/components/Articles/ActganArticle.jsx
@@ -11,8 +11,21 @@ function ActganArticle({ backendURL }) {
   return (
     <div>
       <ArticleHeader info={ActganInfo} />
-      <Paragraph>ACTGAN belongs to a class of machine learning models called GANs (generative adversarial networks) that learn to generate completely novel data (i.e. images) that resemble the training data. ACTGAN is a multiclass-conditioned GAN, meaning it can generate completely different classes (ex. images of dogs, cats, etc.) depending on a class input. It extends on AC-GAN (Auxillary Classifier Generative Adversarial Network)by using a transformer-based architecture for the "discriminator" network. ACTGAN was designed and implemented with a partner as a final project for a machine learning course.</Paragraph>
-      <Paragraph>Several models trained on a variety of datasets are available for demo. The user can generate images by specifying the dataset of the model, the epoch (early epochs are from the beginning of training, while later epochs are from the end of training), the class of image, and an optional seed. </Paragraph>
+      <Paragraph>
+        ACTGAN belongs to a class of machine learning models called GANs (generative adversarial
+        networks) that learn to generate completely novel data (i.e. images) that resemble the
+        training data. ACTGAN is a multiclass-conditioned GAN, meaning it can generate completely
+        different classes (ex. images of dogs, cats, etc.) depending on a class input. It extends
+        on AC-GAN (Auxillary Classifier Generative Adversarial Network) by using a transformer-based
+        architecture for the &quot;discriminator&quot; network. ACTGAN was designed and implemented
+        with a partner as a final project for a machine learning course.
+      </Paragraph>
+      <Paragraph>
+        Several models trained on a variety of datasets are available for demo. The user
+        can generate images by specifying the dataset of the model, the epoch (early epochs are
+        from the beginning of training, while later epochs are from the end of training), the class
+        of image, and an optional seed.
+      </Paragraph>
       <Divider />
       <ModelDemo
         info={ActganInfo}
